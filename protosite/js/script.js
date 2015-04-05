550b19d2c2db51c11657898828c8af0a8b3f03f6
EMario <maryouu@gmail.com>
2015-04-04 22:33:30 -0700

    Minor changes to overhead and alternative style
    Alternative style in which the nav bar is the only fixed element.

--- protosite/js/script.js
+++ protosite/js/script.js
@@ -5,3 +5,11 @@ if(elementExists == null){
 	$('main section').css('width','98%');
 	$('main section').css('margin','0 auto 0 auto');
 }
+
+ 
+if ($(".back_top")[0]){
+	
+} else {
+	$('h2').css('width','98%');
+
+}
